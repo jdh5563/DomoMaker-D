ac3c356b939c996e9d7fd451cb6aeac8c018e240
John Heiden <jdh5563@rit.edu>
2022-03-31 01:25:57 -0400

    Setup all the starter code and modifications

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -4,8 +4,6 @@ const { Account } = models;
 
 const loginPage = (req, res) => res.render('login', { csrfToken: req.csrfToken() });
 
-const signupPage = (req, res) => res.render('signup', { csrfToken: req.csrfToken() });
-
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
@@ -57,10 +55,14 @@ const signup = async (req, res) => {
   }
 };
 
+const getToken = (req, res) => {
+  return res.json({csrfToken: req.csrfToken()});
+};
+
 module.exports = {
   loginPage,
-  signupPage,
   logout,
   login,
   signup,
+  getToken,
 };
